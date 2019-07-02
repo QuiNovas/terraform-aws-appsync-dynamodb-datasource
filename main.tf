8910d0e1667f6a2f3607020abde183544673d50a
Vinay Kumar Chinnakotla <vchinnakotla@quinovas.com>
2019-07-02 16:51:56 +0530

    tfupgrade 0.12

--- main.tf
+++ main.tf
@@ -7,19 +7,19 @@ data "aws_iam_policy_document" "datasource_assume_role" {
       identifiers = [
         "appsync.amazonaws.com",
       ]
-      type        = "Service"
+      type = "Service"
     }
   }
 }
 
 resource "aws_iam_role" "dynamodb_datasource_role" {
-  assume_role_policy = "${data.aws_iam_policy_document.datasource_assume_role.json}"
+  assume_role_policy = data.aws_iam_policy_document.datasource_assume_role.json
   name               = "${var.role_name_prefix}appsync-${local.name}-datasource"
 }
 
 data "aws_iam_policy_document" "dynamodb_access" {
   statement {
-    actions   = [
+    actions = [
       "dynamodb:BatchGetItem",
       "dynamodb:BatchWriteItem",
       "dynamodb:DeleteItem",
@@ -27,30 +27,31 @@ data "aws_iam_policy_document" "dynamodb_access" {
       "dynamodb:PutItem",
       "dynamodb:Query",
       "dynamodb:Scan",
-      "dynamodb:UpdateItem"
+      "dynamodb:UpdateItem",
     ]
     resources = [
-      "${var.dynamodb_table_arn}",
-      "${var.dynamodb_table_arn}/*"
+      var.dynamodb_table_arn,
+      "${var.dynamodb_table_arn}/*",
     ]
-    sid       = "AllowTableAccess"
+    sid = "AllowTableAccess"
   }
 }
 
 resource "aws_iam_role_policy" "dynamo_access" {
-  name    = "${var.dynamodb_table_name}-access"
-  policy  = "${data.aws_iam_policy_document.dynamodb_access.json}"
-  role    = "${aws_iam_role.dynamodb_datasource_role.id}"
+  name   = "${var.dynamodb_table_name}-access"
+  policy = data.aws_iam_policy_document.dynamodb_access.json
+  role   = aws_iam_role.dynamodb_datasource_role.id
 }
 
 resource "aws_appsync_datasource" "dynamodb_datasource" {
-  api_id            = "${var.api_id}"
-  description       = "${var.description}"
+  api_id      = var.api_id
+  description = var.description
   dynamodb_config {
-    region      = "${local.region}"
-    table_name  = "${var.dynamodb_table_name}"
+    region     = local.region
+    table_name = var.dynamodb_table_name
   }
-  name              = "${local.name}"
-  service_role_arn  = "${aws_iam_role.dynamodb_datasource_role.arn}"
-  type              = "AMAZON_DYNAMODB"
+  name             = local.name
+  service_role_arn = aws_iam_role.dynamodb_datasource_role.arn
+  type             = "AMAZON_DYNAMODB"
 }
+
